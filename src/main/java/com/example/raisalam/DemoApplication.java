ee3e8659281ab540df5aa8e437af5747e9ee0247
alamrais <raisa@avaya.com>
2018-09-17 15:55:00 +0530

    Initial commit

--- src/main/java/com/example/raisalam/DemoApplication.java
+++ src/main/java/com/example/raisalam/DemoApplication.java
@@ -2,9 +2,17 @@ package com.example.raisalam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class DemoApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
